25fbe7454179913ee7951625a811f1e635735893
Rahul S Kumar <rahulrockysunil@gmail.com>
2026-02-27 12:41:51 +0530

    refactor: correct typo in DutyInfo component and improve layout in LanguageItem component

--- module/profile/components/LanguageItem.tsx
+++ module/profile/components/LanguageItem.tsx
@@ -18,23 +18,23 @@ const LanguageItem = ({
   active,
 }: LanguageItemProps) => {
   return (
-    <View className="flex flex-row justify-center items-center gap-6 rounded-lg">
-      <TouchableOpacity
-        onPress={() => handleLanguage(language)}
-        activeOpacity={1}
-        className="flex flex-row justify-center items-center gap-3 py-1"
-      >
+    <TouchableOpacity
+      onPress={() => handleLanguage(language)}
+      activeOpacity={1}
+      className="flex-row justify-between items-center py-1"
+    >
+      <View className="flex-row justify-center items-center gap-1">
         <CountryFlag isoCode={country} size={15} />
         <Text className="text-textPrimary font-semibold text-lg w-8">
           {language}
         </Text>
-        <View style={{ width: 16 }}>
-          {active && (
-            <Ionicons name="checkmark" size={15} color={APP_COLORS.white} />
-          )}
-        </View>
-      </TouchableOpacity>
-    </View>
+      </View>
+      <View style={{ width: 18 }}>
+        {active && (
+          <Ionicons name="checkmark" size={20} color={APP_COLORS.primary} />
+        )}
+      </View>
+    </TouchableOpacity>
   );
 };
 
